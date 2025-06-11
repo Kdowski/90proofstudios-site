9909a031cf01057fbb4b85bc47a0f36f5f6e6282
Kdowski <108626050+Kdowski@users.noreply.github.com>
2025-06-10 19:47:18 -0700

    Update prompt_email_util.py

--- prompt_email_util.py
+++ prompt_email_util.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 import os
 
 # Import your new static prompt generator
-from generate_openai_prompt import generate_image_prompt
+from generate_openai_prompt import generate_image_prompts
 
 
 
